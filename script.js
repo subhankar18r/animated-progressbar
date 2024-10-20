d3579d589b9f560b66d1b79621e35011850ae648
Subhankar Rajbanshi <subhankar18r@gmail.com>
2024-10-20 14:11:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const value = document.querySelector("#value");
+const circle = document.querySelector("circle");
+let circumference = circle.getTotalLength();
+let inpValue = document.querySelector("input");
+let count = 0;
+
+function showProgressBar() {
+  if (inpValue.value < 0) inpValue.value = 0;
+  if (inpValue.value > 100) inpValue.value = 100;
+
+  circle.style.strokeDashoffset =
+    circumference - (inpValue.value / 100) * circumference;
+
+  let timer = setInterval(() => {
+    if (count == inpValue.value) {
+      clearInterval(timer);
+      count = 0;
+      return;
+    }
+    count++;
+    value.innerHTML = count + "%";
+  }, 1000 / inpValue.value);
+}
